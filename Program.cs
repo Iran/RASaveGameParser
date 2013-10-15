3826a164b8f069a431cd3069b491e467e34dd5f4
iran <ma1kelvdh@gmail.com>
2013-10-15 13:29:07 +0200

    finish printing code and add extra functions to get player side, savegame version string and whether savegame is multiplayer or not

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.Threading;
+using System.Globalization;
 
 namespace RASaveGameParser
 {
@@ -9,41 +11,57 @@ namespace RASaveGameParser
     {
         static void Main(string[] args)
         {
+            // Make sure we're using en-US culture to prevent weird issues from appearing
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
 
+            string filepath = Environment.CurrentDirectory;
+            DirectoryInfo d = new DirectoryInfo(filepath);
 
-            var SaveGame = new RASaveGame("Savegame.834");
-            Console.ReadLine();
+            Console.WriteLine("Red Alert savegame parser written by Iran.");
+            Console.WriteLine();
+            Console.WriteLine("{0,3} | {1,10} | {2,3} | {3, 8} | {4,5} | {5,19}  | {6,5} |", "Num", "Name",
+                "Lvl", "Side", "Vers", "File Time", "Is MP");
+            Console.WriteLine("--------------------------------------------------------------------------");
+
+            foreach (var file in d.GetFiles("savegame.*"))
+            {
+                var SaveGame = new RASaveGame(file.Name);
+                Console.WriteLine("{0,3:D3} | {1,10} | {2,3} | {3, 8} | {4,5} | {5,19} | {6,5} |", SaveGame.Number, SaveGame.Name,
+                    SaveGame.ScenarioNumber, SaveGame.Get_Player_House(), SaveGame.Get_Version_String(), 
+                    SaveGame.FileTime, SaveGame.Is_Multiplayer());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
+
+            Console.ReadKey();
         }
     }
 
     class RASaveGame
     {
-        string Name = null;
-        int? Number = null;
-        int? ScenarioNumber = null;
-        byte? HouseType = null;
-        int? VersionRawID = null;
-        DateTime? FileTime = null;
+        public string Name = null;
+        public int? Number = null;
+        public int? ScenarioNumber = null;
+        public byte? HouseType = null;
+        public int? VersionRawID = null;
+        public DateTime? FileTime = null;
 
         public RASaveGame(string FilePath)
         {
             string[] SplitFile = FilePath.Split('.');
             this.Number = Int32.Parse(SplitFile[SplitFile.Length - 1]);
 
-            Console.WriteLine(Number);
-
     	    using (BinaryReader b = new BinaryReader(File.Open(FilePath, FileMode.Open)))
 	        {
                 Parse_Save_Game_Name(b);
                 this.ScenarioNumber = b.ReadInt32();
-                Console.WriteLine(ScenarioNumber);
 
                 this.HouseType = b.ReadByte();
                 this.FileTime = File.GetLastWriteTime(FilePath);
 
                 this.VersionRawID = b.ReadInt32();
-                Console.WriteLine("{0:X}",VersionRawID);
-                Console.WriteLine(this.FileTime);
 
 	        }
 	    }
@@ -54,7 +72,55 @@ namespace RASaveGameParser
                 this.Name = Encoding.ASCII.GetString(NameBytes);
 
                 string Gibberish = Encoding.ASCII.GetString(new Byte[] {0x0D, 0x0A, 0x00, 0x1A });
-                this.Name = this.Name.Replace(Gibberish, "");
+                int RemoveIndex =this.Name.IndexOf(Gibberish);
+                this.Name = this.Name.Remove(RemoveIndex);
+
+        }
+
+        public bool Is_Multiplayer()
+        {
+            return this.ScenarioNumber == 0;
+        }
+
+        public string Get_Version_String()
+        {
+            switch (this.VersionRawID)
+            {
+                case 0x100618B: return "3.03";
+                case 0x1007000: return "3.03p";
+
+                default: return "Invalid/Unknown";
+            }
+        }
+
+        public string Get_Player_House()
+        {
+            switch (this.HouseType)
+            {
+                case 0xDD: return "Skirmish";
+                case 0x00: return "Spain";
+                case 0x01: return "Greece";
+                case 0x02: return "USSR";
+                case 0x03: return "England";
+                case 0x04: return "Ukraine";
+                case 0x05: return "Germany";
+                case 0x06: return "France";
+                case 0x07: return "Turkey";
+                case 0x08: return "GoodGuy";
+                case 0x09: return "BadGuy";
+                case 0x0A: return "Neutral";
+                case 0x0B: return "Special";
+                case 0x0C: return "Multi1";
+                case 0x0D: return "Multi2";
+                case 0x0E: return "Multi3";
+                case 0x0F: return "Multi4";
+                case 0x10: return "Multi5";
+                case 0x11: return "Multi6";
+                case 0x12: return "Multi7";
+                case 0x13: return "Multi8";
+
+                default: return "Invalid";
+            }
         }
     }
 }
