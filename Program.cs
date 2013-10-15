b44c7f3a2ad6a8029f87599eda8a377aa54e4a2e
iran <ma1kelvdh@gmail.com>
2013-10-15 11:59:57 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RASaveGameParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+}
